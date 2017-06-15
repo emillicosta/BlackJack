837e4abd1cbe50063d2e68224fe1b5a49b9593b4
Debora Costa <debaemili@gmail.com>
2017-06-15 18:34:49 -0300

    primeiro commit

---
+++ login.php
@@ -0,0 +1,9 @@
+<?php date_default_timezone_set('America/Sao_Paulo'); ?>
+<html>
+<body>
+
+Welcome <?php  echo $_POST["login"]; ?><br>
+Your email address is: <?php echo $_POST["senha"]; ?>
+
+</body>
+</html>
